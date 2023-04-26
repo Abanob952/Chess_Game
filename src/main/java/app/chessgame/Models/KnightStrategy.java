43b93687fca4db35f3a5cc6fdfe53ebba93e7397
DER VARTANIAN Theo <theo.DER-VARTANIAN@etu.univ-amu.fr>
2023-04-26 13:32:03 +0200

    Classe Cavalier

---
+++ src/main/java/app/chessgame/Models/KnightStrategy.java
@@ -0,0 +1,55 @@
+package app.chessgame.Models;
+
+import javafx.scene.paint.Color;
+
+import java.util.*;
+
+
+public class KnightStrategy implements MoveStrategy {
+    private final Color color;
+
+    public KnightStrategy(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * Calculates all the possible moves for a knight from the current cell
+     * @param currentCell
+     * @return returns a list of cells that are valid moves for the knight
+     */
+    @Override
+    public List<Cell> getPossibleMoves(Cell currentCell) {
+        List<Cell> possibleMoves = new ArrayList<>();
+        Map<Color, List<Point>> moves = this.calculateMoves(currentCell.getPoint());
+        for (Point potentialPoint : moves.get(this.color)) {
+            if (Utility.validPoint(potentialPoint)) {
+                possibleMoves.add(Board.getInstance().getCell(potentialPoint));
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    /**
+     * Calculates all the possible moves for a knight from the given point
+     * @param point
+     * @return a map with a single entry for the knight's color, containing a list of possible moves
+     */
+    private Map<Color, List<Point>> calculateMoves(Point point) {
+        return new HashMap<>() {
+            {
+                put(color,
+                        new ArrayList<>(List.of(
+                                new Point(point.getX() + 2, point.getY() - 1),
+                                new Point(point.getX() + 2, point.getY() + 1),
+                                new Point(point.getX() + 1, point.getY() - 2),
+                                new Point(point.getX() + 1, point.getY() + 2),
+                                new Point(point.getX() - 2, point.getY() - 1),
+                                new Point(point.getX() - 2, point.getY() + 1),
+                                new Point(point.getX() - 1, point.getY() - 2),
+                                new Point(point.getX() - 1, point.getY() + 2)
+                        )));
+            }
+        };
+    }
+}
