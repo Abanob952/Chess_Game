b620b4cb21f60c8d3443fcce5c21109753218496
Mufuta Mulamba Nathan <nathanmf06@gmail.com>
2023-04-27 15:59:49 +0200

    Classe BishopStrategy et RookStrategy terminé

---
+++ src/main/java/app/chessgame/Models/RookStrategy.java
@@ -0,0 +1,47 @@
+package app.chessgame.Models;
+
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RookStrategy implements MoveStrategy {
+    private final Color color;
+
+    public RookStrategy(Color color){
+        this.color=color;
+    }
+
+    /**
+     * Calculates all the possible point for a rook
+     * @param currentCell
+     * @return returns a list of cells that are valid moves for this rook
+     */
+    @Override
+    public List<Cell> getPossibleMoves(Cell currentCell) {
+        List<Cell> possibleMoves = new ArrayList<>();
+        Map<Color, List<Point>> moves = this.calculateMoves(currentCell.getPoint());
+        for(Point potentialPoint: moves.get(this.color)){
+            if (Utility.validPoint(potentialPoint)){
+                possibleMoves.add(Board.getInstance().getCell(potentialPoint));
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    private Map<Color, List<Point>> calculateMoves(Point point) {
+        Map<Color, List<Point>> moves = new HashMap<>();
+        List<Point> possibleMoves = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            possibleMoves.add(new Point(point.getX(), point.getY() + i)); // Verticale Haute
+            possibleMoves.add(new Point(point.getX(), point.getY() - i)); //verticale Basse
+            possibleMoves.add(new Point(point.getX() + i, point.getY())); //Horizontale Gauche
+            possibleMoves.add(new Point(point.getX() - i, point.getY())); //Horizontale Droite
+        }
+        moves.put(color, possibleMoves);
+        return moves;
+    }
+}
