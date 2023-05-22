60f7a6cdbe1d24d1b2ea1a48257879eaf82a21a9
ABDELSHAHID Abanoub <abanob.a259@gmail.com>
2023-05-22 19:45:18 +0200

    Implemented bot player

---
+++ src/main/java/app/chessgame/Models/BotPlayer.java
@@ -0,0 +1,25 @@
+package app.chessgame.Models;
+
+import app.chessgame.Models.Events.TurnChangeListener;
+import javafx.scene.paint.Color;
+
+public class BotPlayer extends Player{
+    private final Engine engine;
+    Match match;
+    public BotPlayer(Color color, Match match) {
+        super("Bot", color);
+        this.engine = new Engine(match);
+        this.match = match;
+    }
+
+    @Override
+    public void getPlay() {
+        if(this.match.getTurn().getColor() == this.getColor()){
+            var res = this.engine.generateBestMove(2);
+            this.match.play(res
+                    .getSource(), res.getTarget());
+
+        }
+    }
+
+}
